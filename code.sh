a4f1969e82742a76ee39bd72151f80790e56e04a
neorepo <neorepo.edu@gmail.com>
2021-02-14 09:38:35 -0300

    Actualizando el código

--- code.sh
+++ code.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# declare Associative arrays (like hashmaps): http://wiki.bash-hackers.org/syntax/arrays
 declare -A PROVINCES
 PROVINCES['A']='Salta'
 PROVINCES['B']='Buenos Aires'
@@ -27,21 +26,32 @@ PROVINCES['X']='C\u00F3rdoba'
 PROVINCES['Y']='Jujuy'
 PROVINCES['Z']='Santa Cruz'
 
-rm -rf by-province
+rm -f by-province
 mkdir by-province
 
-echo -e '## Processing all provinces...'
-for i in A B C D E F G H J K L M N P Q R S T U V W X Y Z; do
-  text="{\"iso_31662\":\"AR-${i}\",\"provincia\":\"${PROVINCES[${i}]}\",\"localidades\":"
-  text="${text}`curl -s 'https://www.correoargentino.com.ar/sites/all/modules/custom/ca_forms/api/wsFacade.php' \
-  --data-raw "action=localidades&localidad=none&calle=&altura=&provincia=${i}"`}"
+echo -e "### Processing all provinces..."
 
-  echo '## Saving current province localities...'
-  # tr command needed to delete invisible character on curl
-  # http://alvinalexander.com/blog/post/linux-unix/how-remove-non-printable-ascii-characters-file-unix
+for i in A B C D E F G H J K L M N P Q R S T U V W X Y Z; do
+  echo -e "### Processing province ${PROVINCES[${i}]}"
+  text="{\"iso_31662\":\"AR-${i}\",\"province\":\"${PROVINCES[${i}]}\",\"localities\":"
+  text="${text}`curl -s 'https://www.correoargentino.com.ar/sites/all/modules/custom/ca_forms/api/wsFacade.php' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'Accept-Language: en-US,en;q=0.5' --compressed \
+  -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'Origin: https://www.correoargentino.com.ar' \
+  -H 'DNT: 1' \
+  -H 'Connection: keep-alive' \
+  -H 'Referer: https://www.correoargentino.com.ar/formularios/cpa' \
+  -H 'Cookie: BIGipServerPL_PROD_WWW=3356098826.47873.0000; has_js=1; popup_message_displayed=1612658599580' \
+  --data-raw "action=localidades&localidad=none&calle=&altura=&provincia=${i}"`}"
+  
+  echo '### Saving current province localities...'
   text=$(echo ${text} | tr -cd '\11\12\15\40-\176')
   CURRENT_FILE=$(echo -e "by-province/${PROVINCES[${i}]}.json" | tr -d '[:space:]')
-  # --compact-output: json minified
   echo ${text} | jq --compact-output '.' | cat > ${CURRENT_FILE}
-  echo '## [DONE]'
-done
+  echo '### [DONE]'
+ done
+
+echo '### [COMPLETE]'
